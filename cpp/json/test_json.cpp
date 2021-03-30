6c8dc598cc9a9033d534278106a95a1a63cdee0d
liph <45155620+liphx@users.noreply.github.com>
2021-03-30 16:25:44 +0800

    Update test_json.cpp

--- cpp/json/test_json.cpp
+++ cpp/json/test_json.cpp
@@ -1,11 +1,18 @@
 #include "json.hpp"
-using namespace liph;
+#include <gtest/gtest.h>
 
-#include <iostream>
-using namespace std;
-
-int main()
+TEST(json, constructor)
 {
-    json j = json::parse("{\"name\":\"liph\",\"age\":23}");
-    cout << j.dump() << endl;
+    liph::json j;
+    j["pi"] = 3.14;
+    j["name"] = "liph";
+    j["bool"] = true;
+    j["null"] = nullptr;
+    j["object"]["a"] = "hello";
+    j["object"]["b"] = "world";
+
+    std::string str = R"({"pi": 3.14, "name": "liph", "bool": true, "null": null, "object": {"a": "hello", "b": "world"}})";
+    liph::json j2 = liph::json::parse(str);
+
+    ASSERT_TRUE(j == j2);
 }
