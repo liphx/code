9e1ff0224c8caee63dad2c651f73d40f016fd89b
liph <3138519115@qq.com>
2023-04-14 17:13:57 +0800

    add glog

--- src/cplusplus/examples/main.cpp
+++ src/cplusplus/examples/main.cpp
@@ -1,7 +1,57 @@
-#include "absl/strings/string_view.h"
 #include "liph/liph.h"
-
 using namespace liph;
+
+int main() {}
+
+#if 0
+#include "spdlog/sinks/basic_file_sink.h"
+auto logger = spdlog::basic_logger_mt("basic_logger", "std.log");
+int main() {
+    logger->info("info log");
+    logger->error("info error");
+    sleep(10);
+}
+#endif
+
+#if 0
+#include "glog/logging.h"
+
+int main(int, char **argv) {
+    google::InitGoogleLogging(argv[0]);
+    LOG(INFO) << "info log";
+    LOG(WARNING) << "warning log";
+    LOG(ERROR) << "error log";
+    /* LOG(FATAL) << "fatal log"; */
+    sleep(10);
+}
+#endif
+
+#if 0
+struct A {
+    int x;
+    void f() { delete this; }
+
+    ~A() { std::cout << "~A()" << std::endl; }
+
+    void *operator new(std::size_t size) {
+        std::cout << "A::new" << std::endl;
+        return malloc(size);
+    }
+    void operator delete(void *ptr) {
+        std::cout << "A::delete" << std::endl;
+        free(ptr);
+    }
+};
+
+int main() {
+    A *a = new A;
+    a->f();
+    std::cout << "end main" << std::endl;
+}
+#endif
+
+#if 0
+#include "absl/strings/string_view.h"
 
 int main() {
     D(__GNUC__);
@@ -10,3 +60,4 @@ int main() {
     absl::string_view sv("hello");
     D(sv);
 }
+#endif
