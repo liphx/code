60459925f7a2c35d52faaf5cacd44a0ebcc2bc31
liph <lipenghua@lipenghua.com>
2024-01-10 17:36:23 +0800

    update

--- src/cplusplus/tools/password-generator.cpp
+++ src/cplusplus/tools/password-generator.cpp
@@ -3,7 +3,7 @@
 #include "liph/numeric/random.h"
 
 int main(int argc, char **argv) {
-    int n = 12;
+    int n = 16;  // default length
     if (argc > 1) n = atoi(argv[1]);
     std::cout << liph::random::gen_str(n, "0123456789") << std::endl;
     std::cout << liph::random::gen_str(n,
