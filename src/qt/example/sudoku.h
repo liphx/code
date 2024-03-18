817be0f9d12858ff05f2cc7a366c3353f32b859d
liph <3138519115@qq.com>
2024-03-18 23:37:52 +0800

    update

--- src/qt/example/sudoku.h
+++ src/qt/example/sudoku.h
@@ -7,6 +7,7 @@
 #include <QVBoxLayout>
 #include <cstdlib>
 #include <iostream>
+#include <set>
 #include <vector>
 
 #include "clickable_label.h"
@@ -22,32 +23,66 @@ public:
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
                 labels[i][j] = new ClickableLabel(this);
-                labels[i][j]->setStyleSheet("border: 1px solid;");
+                labels[i][j]->setStyleSheet("border: 1px solid; font-size: 20px;");
                 layout->addWidget(labels[i][j], i, j, 1, 1);
                 QObject::connect(labels[i][j], &ClickableLabel::clicked, [i, j, this]() {
                     // std::cout << "clicked [" << i << "," << j << "]\n";
                     // std::cout << arr_init[i][j] << '\n';
                     if (arr_init[i][j]) return;
+                    delete_selector();
                     selector = new QGridLayout;
                     layout->addLayout(selector, 1, 10, 5, 2);
-                    for (int k1 = 0; k1 < 5; k1++) {
-                        for (int k2 = 0; k2 < 2; k2++) {
-                            auto label = new ClickableLabel;
-                            selector->addWidget(label, k1, k2, 1, 1);
-                            int number = k1 * 2 + k2 + 1;
-                            if (number < 10) {
-                                label->setText(QString::number(number));
-                            } else {
-                                label->setText("clear");
-                                number = 0;
-                            }
-                            QObject::connect(label, &ClickableLabel::clicked, [i, j, number, this]() {
-                                arr[i][j] = number;
-                                delete_selector();
-                                show();
-                            });
+                    std::set numbers{1, 2, 3, 4, 5, 6, 7, 8, 9};
+                    for (int k = 0; k < 9; k++) {
+                        if (k != j && arr[i][k]) {
+                            numbers.erase(arr[i][k]);
                         }
                     }
+                    for (int k = 0; k < 9; k++) {
+                        if (k != i && arr[k][j]) {
+                            numbers.erase(arr[k][j]);
+                        }
+                    }
+                    int k = 0;
+                    for (int number : numbers) {
+                        auto label = new ClickableLabel;
+                        selector->addWidget(label, k++, 1, 1, 1);
+                        label->setText(QString::number(number));
+                        label->setStyleSheet("font-size: 20px;");
+                        QObject::connect(label, &ClickableLabel::clicked, [i, j, number, this]() {
+                            arr[i][j] = number;
+                            delete_selector();
+                            show();
+                        });
+                    }
+                    auto label = new ClickableLabel;
+                    selector->addWidget(label, k, 1, 1, 1);
+                    label->setText("clear");
+                    label->setStyleSheet("font-size: 20px;");
+                    QObject::connect(label, &ClickableLabel::clicked, [i, j, this]() {
+                        arr[i][j] = 0;
+                        delete_selector();
+                        show();
+                    });
+
+                    // for (int k1 = 0; k1 < 5; k1++) {
+                    //     for (int k2 = 0; k2 < 2; k2++) {
+                    //         auto label = new ClickableLabel;
+                    //         selector->addWidget(label, k1, k2, 1, 1);
+                    //         int number = k1 * 2 + k2 + 1;
+                    //         if (number < 10) {
+                    //             label->setText(QString::number(number));
+                    //         } else {
+                    //             label->setText("clear");
+                    //             number = 0;
+                    //         }
+                    //         QObject::connect(label, &ClickableLabel::clicked, [i, j, number, this]() {
+                    //             arr[i][j] = number;
+                    //             delete_selector();
+                    //             show();
+                    //         });
+                    //     }
+                    // }
                 });
             }
         }
@@ -90,6 +125,12 @@ public:
             memset(&arr[0][0], 0, sizeof(arr));
         }
         memcpy(&arr_answer[0][0], &arr[0][0], sizeof(arr));
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                std::cout << arr_answer[i][j] << ' ';
+            }
+            std::cout << std::endl;
+        }
         GenerateProblem();
         memcpy(&arr_init[0][0], &arr[0][0], sizeof(arr));
         show();
@@ -115,7 +156,7 @@ public:
         // Randomly set one for each row, col, grid as unknown
         // repeat
         static const int grid_begin[9][2] = {{1, 1}, {1, 4}, {1, 7}, {4, 1}, {4, 4}, {4, 7}, {7, 1}, {7, 4}, {7, 7}};
-        for (int times = 3; times > 0; times--) {
+        for (int times = 2; times > 0; times--) {
             for (int row = 1; row <= 9; row++) SetUnknownIfUnique(row, rand() % 9 + 1);
             for (int col = 1; col <= 9; col++) SetUnknownIfUnique(rand() % 9 + 1, col);
             for (int grid = 1; grid <= 9; grid++) {
@@ -152,8 +193,39 @@ public:
         return true;
     }
 
+    bool CanFillIn2(int num, int row, int col) const
+    {
+        if (num == 0)
+            return true;
+
+        for (int i = 1; i <= 9; i++) {
+            if (i != col && GetNum(row, i) == num)
+                return false;
+        }
+        for (int i = 1; i <= 9; i++) {
+            if (i != row && GetNum(i, col) == num)
+                return false;
+        }
+
+        int grid_row = 1 + (row - 1) / 3 * 3;
+        int grid_col = 1 + (col - 1) / 3 * 3;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                int x = grid_row + i;
+                int y = grid_col + j;
+                if (x == row && y == col)
+                    continue;
+                if (GetNum(x, y) == num)
+                    return false;
+            }
+        }
+
+        return true;
+    }
+
     bool CanFillIn(int num, int row, int col) const {
-        if (num == 0) return true;
+        if (GetNum(row, col) != 0)
+            return false;
 
         for (int i = 1; i <= 9; i++) {
             if (i != col && GetNum(row, i) == num) return false;
@@ -194,7 +266,7 @@ public:
     void check() {
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
-                if (arr_init[i][j] || arr[i][j] == 0 || CanFillIn(arr[i][j], i + 1, j + 1)) {
+                if (arr_init[i][j] || arr[i][j] == 0 || CanFillIn2(arr[i][j], i + 1, j + 1)) {
                     labels[i][j]->setStyleSheet("border: 1px solid;");
                     continue;
                 }
